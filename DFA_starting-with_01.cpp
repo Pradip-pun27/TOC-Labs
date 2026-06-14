36e9a66f5d3a78f812aded8737700b1d4940cdf7
Pradip-pun27 <pradippoonmgr@gmail.com>
2026-06-14 14:32:26 +0545

    New

--- DFA_starting-with_01.cpp
+++ DFA_starting-with_01.cpp
@@ -1,48 +1,35 @@
-/*
-Name: Pradip-Pun-Magar
-Subject: TOC(4th sem)
-*/
+/* DFA for strings starting with 01
+   Name: Pradip-Pun-Magar | Subject: TOC (4th sem) */
 
 #include <iostream>
 #include <string>
 using namespace std;
+
 int main(){
     string str;
-    int state =0; //Initial State
-
-    cout<<"Enter the string: ";
-    cin>>str;
-
-    //Loop to check the each character of the string for the DFA
-    for (int i=0; i<str.length(); i++){
-        if(str[i] !='0' && str[i]!='1'){
-            cout << "String not accepted.\nPlease enter a string over {0,1}" << endl;
+    int state = 0; // Initial state
+    cout << "DFA: Accept strings starting with 01\n";
+    cout << "Enter string: ";
+    cin >> str;
+    cout << "\nTransition: q0";
+    
+    for(size_t i = 0; i < str.length(); i++){ //size_t: an unsigned int type used for rep.sizes and counts
+        if(str[i] != '0' && str[i] != '1'){
+            cout << "\nInvalid! Only {0,1} allowed\n";
             return 0;
         }
-
-        if (state==0 && str[i]=='0')
-            state=1;
-
-        else if (state==0 && str[i]=='1')
-            state = -1;
+        if(state == 0 && str[i] == '0') state = 1;
+        else if(state == 0 && str[i] == '1') state = -1;
+        else if(state == 1 && str[i] == '0') state = -1;
+        else if(state == 1 && str[i] == '1') state = 2;
+        else if(state == 2 && (str[i]=='0' || str[i]=='1')) state = 2;
         
-        else if (state==1 && str[i]=='0')
-            state = -1;
-        
-        else if (state==1 && str[i]=='1')
-            state = 2;
-        
-        else if (state==2 && (str[i]=='0' || str[i]=='1'))
-            state = 2;
+        cout << " --" << str[i] << "--> ";
+        if(state == 1) cout << "q1";
+        else if(state == 2) cout << "q2";
+        else cout << "D";
     }
-    // check if the string is accepted or not,
-    // i.e. if the final state is 2 then string is accepted
-    // else string is not accepted
-    
-    if (state == 2)
-        cout << "String accepted";
-    else
-        cout << "String not accepted";
-
+    cout << "\n\nResult: ";
+    cout << (state == 2 ? "ACCEPTED" : "REJECTED")<<endl;
     return 0;
 }
